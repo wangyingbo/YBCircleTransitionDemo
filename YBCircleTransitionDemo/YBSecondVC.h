51ac4e159bd2fe38990fe47f8fd9a766f269773e
wangyingbo <wangyingbo528@126.com>
2016-09-13 14:39:12 +0800

    ...

---
+++ YBCircleTransitionDemo/YBSecondVC.h
@@ -0,0 +1,13 @@
+//
+//  YBSecondVC.h
+//  YBCircleTransitionDemo
+//
+//  Created by 王迎博 on 16/9/13.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBSecondVC : UIViewController
+
+@end
--- YBCircleTransitionDemo/YBSecondVC.m
+++ YBCircleTransitionDemo/YBSecondVC.m
@@ -1,53 +1,39 @@
 //
-//  ViewController.m
+//  YBSecondVC.m
 //  YBCircleTransitionDemo
 //
 //  Created by 王迎博 on 16/9/13.
 //  Copyright © 2016年 王迎博. All rights reserved.
 //
 
-#import "ViewController.h"
-#import "YBCircleSpreadTransition.h"
 #import "YBSecondVC.h"
+#import "YBCircleSpreadTransition.h"
 
 
-@interface ViewController ()<UINavigationControllerDelegate>
+@interface YBSecondVC ()
 
 @end
 
-@implementation ViewController
+@implementation YBSecondVC
 
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
-}
-
-
-- (IBAction)click:(UIButton *)sender
-{
-    YBSecondVC *vc = [[YBSecondVC alloc]init];
-    [self.navigationController pushViewController:vc animated:YES];
-    
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 
 #pragma mark - UINavigation Delegate
 -(id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC
 {
-    NSLog(@"...........测试哈哈哈");
     
     if (fromVC == self) {
         return [YBCircleSpreadTransition transitionWithTransitionType:YBCircleSpreadTransitionTypePush];
     }
-    
     if (toVC == self) {
         return [YBCircleSpreadTransition transitionWithTransitionType:YBCircleSpreadTransitionTypePop];
     }
     return nil;
 }
-
 
 
 @end
